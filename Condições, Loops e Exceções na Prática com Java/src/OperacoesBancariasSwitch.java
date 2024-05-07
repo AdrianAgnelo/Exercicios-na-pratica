d3e0b62c0fcca3b7c6d33f639b53b6f5fa65be9f
Adrian <adrianagnelo93@gmail.com>
2024-05-07 20:22:32 -0300

    Finalização

--- Condições, Loops e Exceções na Prática com Java/src/OperacoesBancariasSwitch.java
+++ Condições, Loops e Exceções na Prática com Java/src/OperacoesBancariasSwitch.java
@@ -3,35 +3,59 @@ import java.util.Scanner;
 public class OperacoesBancariasSwitch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); 
+        int opcao;
         double saldo = 0; 
 
-        System.out.println("Bem-vindo ao banco escolha uma opção\n" +
-         "1.Depositar \n" +
-         "2.Sacar \n" +
-         "3.Consultar Saldo \n" +
-         "4.Encerrar \n" );
-        int opcao = scanner.nextInt();
-        
-        
-       do {
+        do {
+            
+            exibirMenu();
+            opcao = lerOpcao();
+
             switch (opcao) {
-                case '1':
-                    System.out.println("olá");
+                case 1:
+                System.out.print("Digite o valor a ser depositado: ");
+                double valorDeposito = scanner.nextDouble();
+                saldo += valorDeposito;
+                System.out.println("Depósito realizado com sucesso! no valor de : " + valorDeposito);
                     break;
-            
+                case 2:
+                    System.out.println("Quanto você deseja sacar ? seu saldo atual é de : " + saldo);
+                    double valorSaque = scanner.nextDouble();
+                    if (valorSaque < saldo) {
+                        saldo -= valorSaque;
+                        System.out.println("Saque de " + valorSaque + " efetuado");
+                    } else {
+                        System.out.println("O valor do saque é maior que o valor do saldo.");
+                    }
+                    break;
+                case 3:
+                    System.out.println("Seu saldo é de : " + saldo);
+                    break;
+                case 4:
+                    System.out.println("Obrigado por usar o Banco Simples!");
+                    break;
                 default:
-                    break;
+                    System.out.println("Opção inválida. Tente novamente.");
             }
-
             
+        } while (opcao != 4);
+    }
 
-        } while (opcao != '4');
+    private static void exibirMenu() {
+        System.out.println("\nBem vindo ao banco publico");
+        System.out.println("\n1. Depositar");
+        System.out.println("2. Sacar");
+        System.out.println("3. Consultar Saldo");
+        System.out.println("4. Sair");
+        System.out.print("\nDigite a opção desejada: ");
+    }
 
-        
-        
-        
-        // Exibe mensagem de opção inválida se o usuário escolher uma opção inválida:    
-              
-            }
-        }
+    private static int lerOpcao() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+
+}
+
 
